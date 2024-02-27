be74b79589919a24c1337b129140f7aae3bf6699
SamuelGSa <sgmsa@icloud.com>
2024-02-27 20:26:45 -0300

    ex013 feito

--- src/aula16e17Exercicios/aula15Exercicios/Exercicio013.java
+++ src/aula16e17Exercicios/aula15Exercicios/Exercicio013.java
@@ -7,6 +7,8 @@ public class Exercicio013 {
 // Faça um programa que peça dois numeros, base e expoente.
 // Calcule e mostre o primeiro número elevado ao segundo numero. não utilize a função de potencia da linguagem.
 
+//    Li o contexto erro e fiz um programa que faça a potencial de qualquer numero, rs
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -16,6 +18,19 @@ public class Exercicio013 {
         System.out.print("Escreva o expoente que deseja: ");
         int expoente = scanner.nextInt();
 
-        System.out.println();
+        int resultado = 0;
+        int resultado2 = 0;
+
+        for (int i = 0; i < expoente; i++){
+            if (i == 0) {
+                resultado = base * base;
+                System.out.println(resultado);
+            } else {
+                resultado2 = resultado * base;
+                resultado = resultado2;
+                System.out.println(resultado2);
+            }
+
+        }
 }
 }
